295d8ccb95642f039ab11623947e89dc47221f78
ivan <ikocherov@mail.ru>
2017-11-28 23:22:29 +0300

    Ilya help me

---
+++ README.md
@@ -0,0 +1,1 @@
+# DRONCHIK
--- testiing.py
+++ testiing.py
@@ -10,3 +10,11 @@ for i in range(-5, 5):
     v = v.replace('x', str(i))
     print(v)
     v = s
+    ''' s = s.replace('-','(0-')
+        while c < len(s):
+            if s[c] == '-':
+                k=c
+                while '0' <= s[k+1]<= '9':
+                    k=k+1
+                s = s[:k+1]+')'+s[k+1:]
+            c+=1'''
